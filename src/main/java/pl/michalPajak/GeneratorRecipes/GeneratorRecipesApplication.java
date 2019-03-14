80939be248e01f3e5a515dcab35549910100b68e
spidomisiek88 <michal.pajak88@gmail.com>
2019-03-14 09:54:44 +0100

    First project commit

---
+++ src/main/java/pl/michalPajak/GeneratorRecipes/GeneratorRecipesApplication.java
@@ -0,0 +1,13 @@
+package pl.michalPajak.GeneratorRecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratorRecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneratorRecipesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/michalPajak/GeneratorRecipes/GeneratorRecipesApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.michalPajak.GeneratorRecipes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GeneratorRecipesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
